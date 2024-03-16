56d185bec0f3388fb50e64499ef01b0444a249bd
Patrick Wallace <patwallace1@prodigy.net>
2024-03-16 00:01:05 -0500

    added eventlisteners, JavaScript functions for dropdown menu items

---
+++ management/static/management/script.js
@@ -0,0 +1,72 @@
+document.addEventListener('DOMContentLoaded', function () {
+
+    // Add event listeners to the dropdown menu items on the management portal
+    document.getElementById('dropdown-add-topic').addEventListener('click', add_topic);
+    document.getElementById('dropdown-edit-topic').addEventListener('click', edit_topic);
+    document.getElementById('dropdown-delete-topic').addEventListener('click', delete_topic);
+
+    document.getElementById('dropdown-add-subtopic').addEventListener('click', add_subtopic);
+    document.getElementById('dropdown-edit-subtopic').addEventListener('click', edit_subtopic);
+    document.getElementById('dropdown-delete-subtopic').addEventListener('click', delete_subtopic);
+
+    document.getElementById('dropdown-add-question').addEventListener('click', add_question);
+    document.getElementById('dropdown-edit-question').addEventListener('click', edit_question);
+    document.getElementById('dropdown-delete-question').addEventListener('click', delete_question);
+
+    document.getElementById('dropdown-add-choice').addEventListener('click', add_choice);
+    document.getElementById('dropdown-edit-choice').addEventListener('click', edit_choice);
+    document.getElementById('dropdown-delete-choice').addEventListener('click', delete_choice);
+
+    //document.querySelector('#sent').addEventListener('click', () => load_mailbox('sent'));
+    //document.querySelector('#archived').addEventListener('click', () => load_mailbox('archive'));
+    //document.querySelector('#compose').addEventListener('click', () => compose_email('compose'));
+    //document.querySelector('#compose-form').addEventListener('submit', submit_email);
+});
+
+function add_topic(){
+    pass
+}
+
+function edit_topic(){
+    pass
+}
+
+function delete_topic(){
+    pass
+}
+
+function add_subtopic(){
+    pass
+}
+
+function edit_subtopic(){
+    pass
+}
+
+function delete_subtopic(){
+    pass
+}
+
+function add_question(){
+    pass
+}
+
+function edit_question(){
+    pass
+}
+
+function delete_question(){
+    pass
+}
+
+function add_choice(){
+    pass
+}
+
+function edit_choice(){
+    pass
+}
+
+function delete_choice(){
+    pass
+}
--- management/views.py
+++ management/views.py
@@ -2,10 +2,13 @@ from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseRedirect
 from django.contrib import messages
 
-#from .models import User
+from .models import Topic, Subtopic
 
 def management_portal(request):
    
     return render(request, 'management/manage_db.html')
+
+def add_topic(request):
+    pass
 
 
